029ba8a383492ed400e4da819e1192bc1db3f97c
joelee <lizhou1021@qq.com>
2024-01-07 19:52:41 +0800

    feat: 生成随机场景

--- examples/App.tsx
+++ examples/App.tsx
@@ -1,31 +1,57 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "./App.css";
 import Konva from "konva";
 import { List } from "./List";
+import { Rect } from "konva/lib/shapes/Rect";
+import { Circle } from "konva/lib/shapes/Circle";
+import { Arc } from "konva/lib/shapes/Arc";
+import { Ellipse } from "konva/lib/shapes/Ellipse";
+
+//获取min~max之间的随机整数
+function random(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>();
+
   useEffect(() => {
+    if (!containerRef.current) return;
     const stage = new Konva.Stage({
-      container: "konvajs", // id of container <div>
-      width: 500,
-      height: 500,
+      container: containerRef.current, // id of container <div>
+      width: containerRef.current.clientWidth,
+      height: containerRef.current.clientHeight,
     });
+
+    const count = random(500, 1000);
 
     // then create layer
     const layer = new Konva.Layer();
 
-    // create our shape
-    const circle = new Konva.Circle({
-      x: stage.width() / 2,
-      y: stage.height() / 2,
-      radius: 70,
-      fill: "red",
-      stroke: "black",
-      strokeWidth: 4,
-    });
+    const CLASSES = [Rect, Circle, Arc, Ellipse];
+    const Colors = ["red", "blue", "green", "yellow", "purple", ""];
 
-    // add the shape to the layer
-    layer.add(circle);
+    for (let i = 0; i < count; i++) {
+      const CLASS = CLASSES[random(0, CLASSES.length - 1)];
+      const color = Colors[random(0, Colors.length - 1)];
+      const color2 = Colors[random(0, Colors.length - 1)];
+
+      const circle = new CLASS({
+        x: random(0, stage.width()),
+        y: random(0, stage.height()),
+        radius: random(1, 10),
+        angle: random(0, 360),
+        rotation: random(0, 360),
+        radiusX: random(1, 50),
+        radiusY: random(1, 50),
+        innerRadius: random(1, 30),
+        outerRadius: random(30, 50),
+        fill: color,
+        stroke: color2,
+        strokeWidth: random(1, 10),
+      });
+      layer.add(circle);
+    }
 
     // add the layer to the stage
     stage.add(layer);
@@ -37,7 +63,7 @@ function App() {
 
   return (
     <div className="w-full h-full flex">
-      <div id="konvajs" className="w-8/12 outline"></div>
+      <div id="konvajs" className="w-8/12 outline" ref={containerRef}></div>
       <div className="flex-1">
         <List />
       </div>
